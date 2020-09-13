67d06d08aa2dd4d8499bf41ce9d7e03212bb658c
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-09-13 18:28:25 +0300

    Reactive form with validations created

--- Login-Form/src/app/login/login.component.ts
+++ Login-Form/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { ConfirmedValidator } from './confirm-password';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  phonePrefixes: string[];
+  possitions: string[];
+  form;
+
+  constructor(private fb: FormBuilder) {
+    this.phonePrefixes = ['+359', '+324', '+213'];
+    this.possitions = ['Developer', 'Designer', 'Manager']
+   }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      fullName: ['', [Validators.required, Validators.pattern('([A-Z][a-z]+)\\s([A-Z][a-z]+)')]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[\\d]{9}$')]],
+      possition: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['']
+    }, {
+  
+      validator: ConfirmedValidator('password', 'confirmPassword')
+    });
+  }
+
+ get f() {
+   return this.form.controls;
+  }
+
+  register() {
+    if (this.form.valid) {
+      debugger
+      this.form.reset();
+    }
+    console.log(this.form);
   }
 
 }
