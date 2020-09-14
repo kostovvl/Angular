5a556105055c2866cce789b74de14e4916319a56
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-09-14 19:30:08 +0300

    All categories added to client

--- Movies/movie-finder/src/app/movies/movies.component.ts
+++ Movies/movie-finder/src/app/movies/movies.component.ts
@@ -12,11 +12,27 @@ export class MoviesComponent implements OnInit {
   constructor(private moivesService: MoviesService) { }
 
   popularMovies : Movie[];
+  topRatedMovies: Movie[];
+  nowPlayingMovies: Movie[];
+  upcomingMovies: Movie[];
+
   foundMovies: Movie[];
 
   ngOnInit(): void {
     this.moivesService.getPopularMovies().subscribe(data => {
       this.popularMovies = data.slice(0, 6);
+    })
+
+    this.moivesService.getTopRatedMovies().subscribe(data => {
+      this.topRatedMovies = data.slice(0, 6);
+    })
+
+    this.moivesService.getNowPlayingMovies().subscribe(data => {
+      this.nowPlayingMovies = data.slice(0, 6);
+    })
+
+    this.moivesService.getUpcoming().subscribe(data => {
+      this.upcomingMovies = data.slice(0, 6);
     })
   }
 
--- Movies/movie-finder/src/app/service/movies.service.ts
+++ Movies/movie-finder/src/app/service/movies.service.ts
@@ -6,6 +6,11 @@ import Movie from '../model/movie.model';
 import MovieDetails from '../model/movie.details';
 
 const POULAR_URL = 'http://localhost:8080/popular'
+const TOP_RATED_URL = 'http://localhost:8080/top_rated'
+const NOW_PLAYING_URL = 'http://localhost:8080/now_playing'
+const UPCOMING_URL = 'http://localhost:8080/upcoming'
+
+
 const DETAILS_URL = 'http://localhost:8080/details/'
 const SEARCH_URL = 'http://localhost:8080/search/'
 
@@ -22,6 +27,19 @@ export class MoviesService {
   getPopularMovies() {
     return this.httpClient.get<Movie[]>(POULAR_URL);
   }
+
+  getTopRatedMovies() {
+    return this.httpClient.get<Movie[]>(TOP_RATED_URL);
+  }
+
+  getNowPlayingMovies() {
+    return this.httpClient.get<Movie[]>(NOW_PLAYING_URL);
+  }
+
+  getUpcoming() {
+    return this.httpClient.get<Movie[]>(UPCOMING_URL);
+  }
+
 
   getDetails(id: number) {
     return this.httpClient.get<MovieDetails>(DETAILS_URL + id);
