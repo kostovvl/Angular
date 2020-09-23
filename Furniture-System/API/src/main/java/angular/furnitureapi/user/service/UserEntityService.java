af26224db520314290fb80966cde219e720789f4
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-09-23 14:56:49 +0300

    API signup and login

--- Furniture-System/API/src/main/java/angular/furnitureapi/user/service/UserEntityService.java
+++ Furniture-System/API/src/main/java/angular/furnitureapi/user/service/UserEntityService.java
@@ -1,8 +1,11 @@
 package angular.furnitureapi.user.service;
 
+import angular.furnitureapi.user.domain.UserEntity;
+import angular.furnitureapi.user.domain.UserEntityDto;
 import angular.furnitureapi.user.repository.UserEntityRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,10 +13,18 @@ public class UserEntityService {
 
     private final UserEntityRepository userEntityRepository;
     private final ModelMapper mapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserEntityService(UserEntityRepository userEntityRepository, ModelMapper mapper) {
+    public UserEntityService(UserEntityRepository userEntityRepository, ModelMapper mapper, PasswordEncoder passwordEncoder) {
         this.userEntityRepository = userEntityRepository;
         this.mapper = mapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserEntityDto signUp(UserEntityDto newUserDto) {
+        UserEntity newUser = this.mapper.map(newUserDto, UserEntity.class);
+        newUser.setPassword(this.passwordEncoder.encode(newUserDto.getPassword()));
+        return this.mapper.map(this.userEntityRepository.saveAndFlush(newUser), UserEntityDto.class);
     }
 }
--- Furniture-System/API/src/main/java/angular/furnitureapi/user/web/UserController.java
+++ Furniture-System/API/src/main/java/angular/furnitureapi/user/web/UserController.java
@@ -1,9 +1,27 @@
 package angular.furnitureapi.user.web;
 
+import angular.furnitureapi.user.domain.UserEntityDto;
+import angular.furnitureapi.user.service.UserEntityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class UserController {
+
+    private final UserEntityService userEntityService;
+
+    @Autowired
+    public UserController(UserEntityService userEntityService) {
+        this.userEntityService = userEntityService;
+    }
+
+    @PostMapping("/register")
+    public UserEntityDto signUp(@RequestBody UserEntityDto newUser) {
+        return this.userEntityService.signUp(newUser);
+    }
+
 }
