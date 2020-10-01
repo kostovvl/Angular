2adde66a30a2e7bb53eb08fbfcc94228a00fa42f
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-10-01 13:49:34 +0300

    AuthGuard

--- SeenIt/Client/src/app/app-routing.module.ts
+++ SeenIt/Client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
+import { AuthGuard } from './core/guards/authGuard';
 
 
 
@@ -9,7 +10,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'posts', loadChildren: './posts/posts.module#PostsModule' },
+  { path: 'posts', loadChildren: './posts/posts.module#PostsModule', canLoad: [AuthGuard]},
  
 ];
 
