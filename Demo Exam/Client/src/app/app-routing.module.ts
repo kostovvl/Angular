a3f405c81ea3bc6c1ae4aff7bfc9adb375f3a82b
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-10-06 15:10:38 +0300

    Shared Module + Authentication Module with lazy loading

--- Demo Exam/Client/src/app/app-routing.module.ts
+++ Demo Exam/Client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: '', pathMatch: 'full', redirectTo: 'home'},
+ {path: 'auth', loadChildren: './components/authentication/authentication.module#AuthenticationModule' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Demo Exam/Client/src/app/app.module.ts
+++ Demo Exam/Client/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
