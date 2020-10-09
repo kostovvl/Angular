2e393c0819ab0a94b9161deb52815b43854cd639
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-10-09 14:43:10 +0300

    Categoty delete only by admin

--- Demo Exam/Client/src/app/components/category/all-categories/all-categories.component.ts
+++ Demo Exam/Client/src/app/components/category/all-categories/all-categories.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CategoryService } from 'src/app/core/service/category.service';
 import Category from 'src/app/core/model/category.model';
+import { AuthService } from 'src/app/core/service/auth.service';
 
 @Component({
   selector: 'app-all-categories',
@@ -12,12 +13,35 @@ export class AllCategoriesComponent implements OnInit {
 
   allCategories$ : Observable<Category[]>
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(
+    private categoryService: CategoryService,
+    private authService: AuthService
+    ) { }
 
   ngOnInit(): void {
 
     this.allCategories$ = this.categoryService.all();
 
+  }
+
+  canDelete() {
+    if (this.authService.isAuthenticated()) {
+      if (this.authService.isAdmin()) {
+        return true;
+      }
+      return false;
+    } 
+    return false;
+  }
+
+
+
+  delete(id: number) {
+    this.categoryService.delete(id)
+    .subscribe(data => {
+      this.ngOnInit();
+    })
+    
   }
 
 }
--- Demo Exam/Client/src/app/core/service/category.service.ts
+++ Demo Exam/Client/src/app/core/service/category.service.ts
@@ -7,12 +7,17 @@ import Category from '../model/category.model'
 })
 export class CategoryService {
 
-  private readonly all_categories_url = 'http://localhost:8080/categories/all'
+  private readonly all_categories_url = 'http://localhost:8080/categories/all';
+  private readonly delete_url = 'http://localhost:8080/categories/delete/'
 
   constructor(private http: HttpClient) { }
 
   all() {
     return this.http.get<Category[]>(this.all_categories_url);
+  }
+
+  delete(id: number) {
+    return this.http.delete(this.delete_url + id);
   }
 
 }
